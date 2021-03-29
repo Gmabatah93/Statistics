3405811b6386f79e6328d5811bdff12bcdf86a42
Isioma Mabatah <Mabatah93@gmail.com>
2021-03-29 18:32:41 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# Statistics
-How to use Statistics in R
+# Experimental Design
+### Principals
+>* **Control**: compare treatment to control group
+* **Replicate**: large Sample or Replicate
+* **Randomize**: Randomly assign subjects to treatments
+* **Block**: Covariance Analysis
+  + Randomized Complete Block Design:
+    + Each Block sees each treatment once.
+    + Random within each block
+  + Balanced Incomplete Block Design
+    + not every treatment will appear in every block
+  + Latin Squares
+    + 2 blocking variables
+    + same levels
+    + No Interactions
+  + Factorial Designs
+    + Interactions Considered
+
+### Sampling
+> If Sampling of sample size **n** is RANDOM, then the sample is UNBIASED & REPRESENTATIVE of the population. \
+Thus, any results based on the sample can GENERALIZE to the population parameter
+
+> **Simple Random Sample**:\
+**Stratified Sample**:\
+**Clustered Sample**:\
+**Multistage Sample**:
+
+>**Bootstrap**: Sampling with Replacement. *mimics Sampling Distribution using a single sample*\
+**Permutation**: Sampling without Replacement. _shuffled dataset assuming Null is true_
+
+
+### Testing Procedure
+>1. Well Developed, Clear Research Problem
+2. Establish Null
+3. Determine Appropriate Statistical Test & Sampling Distribution
+4. Choose Type I Error rate "alpha"
+5. State Decision Rule
+6. Gather Sample Data
+7. Calculate Test Statistic
+8. State Statistical Conclusion
+9. Make Decision of Inference Based on Conclusion
+
+---
+
+# Hypothesis Testing
+> "Does our conclusion match the overall state of Reality"\
+"Did our sample come from the same population we assume is underlying the Null"
+
+**Null**: Status Quo - Assumed - Given\
+**Alternative**: Claim - Assertion - Unknown
+
+Null = True | Alt = True
+ --- | ---
+x correctly inside non-reject region | x inside non-reject region due to chance. **Type II - B**
+x outside non-rejection region due to chance. **Type I - a** | x correctly outside nonrejection Region. **Power: 1 - B**
+
+<p float="left">
+  <img src="Images/TYPE1.PNG" width="400" />
+  <img src="Images/TYPE2.PNG" width="400" />
+</p>
+
+---
+
+# Inference
---
+++ statistics.R
@@ -0,0 +1,91 @@
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(infer)
+
+# NUMERICAL INFERENCE: Single Parameter ----
+
+# On a given day, 20 1 bedroom apartments were randomly selected on Craiglist Manhattan from apartments listed "by owner". Is the mean of the median a better measure of typical rent in Manhattan ?
+# Data
+manhat <- read_csv("Data/manhattan.csv")
+# EDA
+# - median
+median_hat <- manhat %>% 
+  specify(response = rent) %>% 
+  calculate(stat = "median") %>% 
+  pull
+# - visual
+manhat %>% 
+  ggplot(aes(rent)) +
+  geom_histogram(bins = 20) +
+  geom_vline(xintercept = median_hat, color = "red")
+
+# Confidence Interval
+# - 15,000 bootsrap samples 
+median_boot <- manhat %>% 
+  specify(response = rent) %>% 
+  generate(reps = 15000, type = "bootstrap") %>% 
+  calculate(stat = "median")
+# - Percentile Method
+median_boot %>% get_confidence_interval(type = "percentile")
+# - Standard Error Method
+median_boot %>% get_confidence_interval(type = 'se', point_estimate = median_hat)
+
+# Hypothesis Test: Rent < $2,500
+# - 15,000 bootstrap distibution centered at null
+median_null_boot <- manhat %>% 
+  specify(response = rent) %>% 
+  hypothesize(null = "point", med = 2500) %>% 
+  generate(reps = 15000, type = "bootstrap") %>% 
+  calculate(stat = "median")
+# - pvalue
+median_null_boot %>% get_p_value(obs_stat = median_hat, direction = "left")
+median_null_boot %>% visualise() + shade_p_value(obs_stat = median_hat, direction = "left")
+
+# Conclusion
+# - With a 95% we reject the H0
+# - We are 95% Confidence that the true Median Rent in Man is btw [2010, 2690]
+
+
+
+
+# The state of NC released to the public a large dataset containing information on births recorded in this state. 
+# The dataset has been of interest to medical researchers studying the relation between habits and practices of expectant mothers and the birth of their children. 
+
+# Data
+ncbirths <- openintro::ncbirths
+# - clean: NAs
+ncbirths_complete <- ncbirths %>% filter(!is.na(visits))
+
+# Confidence Interval
+# - 15,000 bootstrap distibution
+visits_boot <- ncbirths_complete %>% 
+  specify(response = visits) %>% 
+  generate(reps = 15000, type = "bootstrap") %>% 
+  calculate(stat = "mean")
+
+# Hypothesis Test: Birth Weigh > 7lbs
+# - 15,000 bootstrap centered around the null
+weight_null_boot <- ncbirths_complete %>% 
+  specify(response = visits) %>% 
+  hypothesize(null = "point", mu = 7) %>% 
+  generate(reps = 1500, type = "bootstrap") %>% 
+  calculate(stat = "mean")
+# - observed mean
+weight_mean_hat <- ncbirths_complete %>%
+  specify(response = weight) %>% 
+  calculate(stat = "mean")
+# - p value
+weight_null_boot %>% visualise(obs_stat = weight_mean_hat, direction = "right")
+weight_null_boot %>% get_p_value(obs_stat = weight_mean_hat, direction = "right")
+# Confidenct Interval
+x_boot <- ncbirths_complete %>% 
+  specify(response = weight) %>%
+  generate(reps = 1500, type = "bootstrap") %>% 
+  calculate(stat = "mean")
+x_boot %>% get_confidence_interval(type = "se", point_estimate = x_hat, level = 0.95)
+x_boot %>% visualise() + shade_ci(endpoints = c(7.02, 7.20))
+# Conclusion
+# - With a 95% Confidence we REJECT the null Hypothesis
+# - We are 95% Confidence the True mean lies between [,]
+
